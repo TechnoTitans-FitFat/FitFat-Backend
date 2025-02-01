240e056c9d1d913223a34d4cca4f14addabf1ab1
Moetaz-Alfy <workalfy4@gmail.com>
2025-02-02 01:03:25 +0200

    Pagination feature on recipe

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -4,9 +4,23 @@ const { searchHistory } = require("../middleware/searchHistory");
 module.exports = {
   getAllRecipes: async (req, res) => {
     try {
-      const recipes = await Recipe.find({});
+      const { page = 1, limit = 10 } = req.query;
+      const skip = (page - 1) * limit;
+
+      //note: we need to determine how many documents to skip before fetching the next set of results.
+      // This is where the skip calculation comes in.!!
+
+      const recipes = await Recipe.find({}).skip(skip).limit(parseInt(limit));
+      const totalRecipes = await Recipe.countDocuments();
+
       const updatedRecipes = await res.locals.addFields(recipes);
-      res.status(200).json(updatedRecipes);
+
+      res.status(200).json({
+        totalRecipes,
+        totalPages: Math.ceil(totalRecipes / limit),
+        currentPage: parseInt(page),
+        recipes: updatedRecipes,
+      });
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: "Server error" });
@@ -14,7 +28,8 @@ module.exports = {
   },
 
   searchRecipes: async (req, res) => {
-    const { name } = req.query;
+    const { name, page = 1, limit = 10 } = req.query;
+    const skip = (page - 1) * limit;
 
     if (!name) {
       return res.status(200).json({ searchHistory });
@@ -23,6 +38,12 @@ module.exports = {
     try {
       const recipes = await Recipe.find({
         name: { $regex: name, $options: "i" },
+      })
+        .skip(skip)
+        .limit(parseInt(limit));
+
+      const totalRecipes = await Recipe.countDocuments({
+        name: { $regex: name, $options: "i" },
       });
 
       if (recipes.length === 0) {
@@ -30,7 +51,13 @@ module.exports = {
       }
 
       const updatedRecipes = await res.locals.addFields(recipes);
-      res.status(200).json(updatedRecipes);
+
+      res.status(200).json({
+        totalRecipes,
+        totalPages: Math.ceil(totalRecipes / limit),
+        currentPage: parseInt(page),
+        recipes: updatedRecipes,
+      });
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: "Server error" });
