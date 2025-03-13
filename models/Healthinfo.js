cca8af4e916b517cb4b03880a390a16a6967c0c2
Moetaz-Alfy <workalfy4@gmail.com>
2025-03-13 12:29:00 +0200

    unable the required

--- models/Healthinfo.js
+++ models/Healthinfo.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const healthInfoSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  gender: { type: String, required: true, enum: ["female", "male"] },
-  dateOfBirth: { type: Date, required: true },
-  weight: { type: Number, required: true },
-  height: { type: Number, required: true },
+  gender: { type: String, enum: ["female", "male"] },
+  dateOfBirth: { type: Date },
+  weight: { type: Number },
+  height: { type: Number },
   foodAllergies: [{ type: String }],
   diabetes: { type: Boolean, default: false },
   diabetesType: { type: String, enum: ["Type 1", "Type 2"] },
